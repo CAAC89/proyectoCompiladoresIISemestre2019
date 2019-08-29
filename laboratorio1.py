5e6b4dc8911fca227c3efea2ffae11f50914823d
pkJefri <pk_jefri@hotmail.com>
2019-08-29 16:04:24 -0600

    Actualización

--- laboratorio1.py
+++ laboratorio1.py
@@ -1,7 +1,9 @@
 import re
 
+# Clase scanner
 class scanner():
     def __init__(self):
+        # Se definen las expresiones regulares de los tokens y variables
         self.LISTARE = []
         self.INICIO = re.compile('(Del|De la)\s')
         self.LUGAR = re.compile('"([a-z]|\s)+"')
@@ -10,12 +12,14 @@ class scanner():
         self.CONECTORES = re.compile('(y|hasta|si no|hacia el|hacia la|hasta la)\s')
         self.CARDINALES = re.compile('(norte|sur|este|oeste|arriba|abajo|derecha|izquierda)')
         self.DETALLES = re.compile('([a-z]|\s)+')
-        self.PUNTO = re.compile("\.")
+        self.FIN = re.compile("(\.|\n)+")
         self.TEXTO = ''
         self.TEXTORIGINAL = ''
         self.TOKENS = []
 
+    # Método agregar, agrega las expresiones regulares a una lista 
     def agregar(self):
+        # Se lee el archivo de entrada
         archivo = ""
         while(archivo == ""):
             try:
@@ -30,12 +34,14 @@ class scanner():
         self.LISTARE.append((self.MEDIDA,"Medida"))
         self.LISTARE.append((self.CONECTORES,"Conectores"))
         self.LISTARE.append((self.CARDINALES,"cardinales"))
+        self.LISTARE.append((self.FIN,"Fin"))
         self.LISTARE.append((self.DETALLES,"Detalles"))
-        self.LISTARE.append((self.PUNTO,"Punto"))
 
-    def todo(self):
+    # Método que genera los tokens a partir de las expresiones regulares
+    def generador_tokens(self):
         i = 0
         largo = len(self.LISTARE)
+        # Se buscan coincidencias de las expresiones regulares 
         while(i < largo):
             if(self.LISTARE[i][0].search(self.TEXTO) != None):
                 while(self.LISTARE[i][0].search(self.TEXTO) != None):
@@ -45,16 +51,25 @@ class scanner():
                     self.TEXTO = self.TEXTO.replace(token,cambio,1)
                 i += 1
             else:
-                print("Error", self.LISTARE[i][1])
+                print("Error: No se encuentra ningún token de tipo <", self.LISTARE[i][1],">")
                 i += 1
-				
+        # Se verifica si en el texto quedan caracteres inválidos 
+        patron = re.compile('.+')
+        self.TEXTO = self.TEXTO.replace("#","\n")
+        for m in patron.finditer(self.TEXTO):
+            print("Error: El caracter <",m.group(),"> no es válido. Posición inicial:",m.start(),"- Posición final:",m.end())
 
-    def error(self, detalle):
-        print("Error de token",detalle)
+    # Función que imprime los tokens en pantalla
+    def imprimir_tokens(self):
+        print()
+        print("Lista de TOKENS")
+        print()
+        for token in self.TOKENS:
+            print('<"'+token[0]+'", "'+token[1]+'">')
         
-
+# Función principal
 if(__name__ == '__main__'):
     scanner = scanner()
     scanner.agregar()
-    scanner.todo()
-    print(scanner.TOKENS)
+    scanner.generador_tokens()
+    scanner.imprimir_tokens()
